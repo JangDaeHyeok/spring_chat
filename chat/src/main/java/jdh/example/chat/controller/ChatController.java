811ffb1f6cfa131c85ca3565393075949117724e
JandDaeHyeok <jangdaehyeok@gmail.com>
2022-01-17 00:49:17 +0900

    채팅 컨트롤러 수정

--- chat/src/main/java/jdh/example/chat/controller/ChatController.java
+++ chat/src/main/java/jdh/example/chat/controller/ChatController.java
@@ -7,12 +7,14 @@ import jdh.example.chat.model.dto.ChatMsgDTO;
 import jdh.example.chat.model.repository.ChatRoomRepository;
 import jdh.example.chat.redis.RedisPublisher;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author 장대혁
  * @date 2022-01-15
  * @description 
  */
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
@@ -21,13 +23,17 @@ public class ChatController {
 	
 	// WebSocket /pub/chat/message 로 들어오는 메시지 처리
 	@MessageMapping("/chat/message")
-	public void message(ChatMsgDTO chatMsgDTO) {
+	public void message(ChatMsgDTO chatMsgDTO) throws Exception {
 		if(ChatMsgDTO.MessageType.ENTER.equals(chatMsgDTO.getType())) {
 			chatRoomRepository.enterChatRoom(chatMsgDTO.getRoomId());
 			chatMsgDTO.setMessage(chatMsgDTO.getSender() + "님이 입장하셨습니다.");
 		}
 		
-		// WebSocket에 발행된 메시지를 redis로 발행(publish)
-		redisPublisher.publish(chatRoomRepository.getTopic(chatMsgDTO.getRoomId()), chatMsgDTO);
+		try {
+			// WebSocket에 발행된 메시지를 redis로 발행(publish)
+			redisPublisher.publish(chatRoomRepository.getTopic(chatMsgDTO.getRoomId()), chatMsgDTO);
+		} catch (NullPointerException e) {
+			log.error("[ChatController] 채팅 전송 중 문제 발생 : ", e.getMessage());
+		}
 	}
 }
