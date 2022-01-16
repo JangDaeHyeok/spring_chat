ad4607e48ae4bc530071b25ec9e6d3e577ee02ee
JandDaeHyeok <jangdaehyeok@gmail.com>
2022-01-17 00:46:31 +0900

    Embedded Redis 제거

--- chat/src/main/java/jdh/example/chat/config/redis/EmbeddedRedisConfig.java
+++ chat/src/main/java/jdh/example/chat/config/redis/EmbeddedRedisConfig.java
@@ -1,37 +1,30 @@
 package jdh.example.chat.config.redis;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import redis.embedded.RedisServer;
+// import redis.embedded.RedisServer;
 
 /**
  * @author 장대혁
  * @date 2022-01-15
  * @description 로컬 환경일 경우 내장 레디스를 실행하는 config
  */
-@Profile("local")
-@Configuration
-public class EmbeddedRedisConfig {
-	@Value("${spring.redis.port}")
-	private int redisPort;
-	
-	private RedisServer redisServer;
-	
-	@PostConstruct
-	public void redisServer() {
-		redisServer = new RedisServer(redisPort);
-		redisServer.start();
-	}
-	
-	@PreDestroy
-	public void stopRedisServer() {
-		if(redisServer != null) {
-			redisServer.stop();
-		}
-	}
-}
+//@Profile("local")
+//@Configuration
+//public class EmbeddedRedisConfig {
+//	@Value("${spring.redis.port}")
+//	private int redisPort;
+//	
+//	private RedisServer redisServer;
+//	
+//	@PostConstruct
+//	public void redisServer() {
+//		redisServer = new RedisServer(redisPort);
+//		redisServer.start();
+//	}
+//	
+//	@PreDestroy
+//	public void stopRedisServer() {
+//		if(redisServer != null) {
+//			redisServer.stop();
+//		}
+//	}
+//}
