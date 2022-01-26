d6ae7cf61edecfa5a9ae95ec3588c5f5b9514393
JangDaeHyeok <JangDaeHyeok@gmail.com>
2022-01-26 17:49:48 +0900

    redis config 수정(비밀번호)

--- chat/src/main/java/jdh/example/chat/config/redis/RedisConfig.java
+++ chat/src/main/java/jdh/example/chat/config/redis/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -21,10 +22,27 @@ public class RedisConfig {
 	private String redisHost;
 	@Value("${spring.redis.port}")
 	private int redisPort;
+	@Value("${spring.redis.password}")
+	private String redisPassword;
 	
-	@Bean
+	/* 비밀번호가 없는 경우
+	 * @Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+	*/
+	
+	/*
+	 * 비밀번호가 있는 경우
+	 */
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(redisHost);
+		redisStandaloneConfiguration.setPort(redisPort);
+		redisStandaloneConfiguration.setPassword(redisPassword);
+		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+		return lettuceConnectionFactory;
 	}
 	
 	// redis pub/sub 메시지를 처리하는 listner
